debda7018865e0246d345ece11fca6184ffb2926
istrk33 <ilkerstrk33@gmail.comm>
2022-06-21 15:42:59 +0200

    test other version

--- listeners/textFields/searchTextChanged.js
+++ listeners/textFields/searchTextChanged.js
@@ -9,11 +9,13 @@ const service = require("../../services/userDataService");
  * @returns 
  */
 module.exports = async (_props, event, api) => {
-    var obj = (await service.getGeneral(api)).data.data[0];
-    var id = obj._id;
-    var datas = obj;
-    datas["searchValue"] = event.value;
-    return service.put(api, id, datas).then(function (response) {
+    var obj = (await service.getDatastore(api, "vars")).data.data;
+    console.log(obj);
+    // var obj = (await service.getGeneral(api)).data.data[0];  getDatastore(api, name) {
+    var id = obj[0]._id;
+    var data = obj[0].data;
+    data.searchValue = event.value;
+    return service.put(api, id, data).then(function (response) {
         response.data
     });
 }
